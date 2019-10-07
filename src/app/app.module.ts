db7417d798886e38a865c493a5c009a139484723
ImsirovicAjdin <ajdika@live.com>
2019-10-07 07:50:17 +0200

    Add Jumbotron component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app-component/app.component';
 import { NavbarComponent } from './navbar-component/navbar.component';
+import { JumbotronComponent } from './jumbotron-component/jumbotron.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, NavbarComponent ],
+  declarations: [ AppComponent, NavbarComponent, JumbotronComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
