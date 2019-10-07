921e080209d8d22ab20a24eccb662f97cd950eeb
ImsirovicAjdin <ajdika@live.com>
2019-10-07 07:45:04 +0200

    Add the navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app-component/app.component';
+import { NavbarComponent } from './navbar-component/navbar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, NavbarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
