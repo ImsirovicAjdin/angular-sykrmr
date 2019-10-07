ee84940b9641280832edbb92352bcc21a2c80a05
ImsirovicAjdin <ajdika@live.com>
2019-10-07 07:54:39 +0200

    Add the cards component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app-component/app.component';
 import { NavbarComponent } from './navbar-component/navbar.component';
 import { JumbotronComponent } from './jumbotron-component/jumbotron.component';
+import { CarsComponent } from './cards-component/cards.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, NavbarComponent, JumbotronComponent ],
+  declarations: [ AppComponent, NavbarComponent, JumbotronComponent, CarsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
